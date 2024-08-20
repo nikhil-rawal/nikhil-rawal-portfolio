58798ab972efb7201361f7af7b7dd69ad1899d7c
Nikhil Rawal <59576734+nikhil-rawal@users.noreply.github.com>
2024-08-19 23:51:12 -0400

    react-icons checked

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,18 +1,23 @@
 import React from "react";
+import { FaSun } from "react-icons/fa6";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 const Head = () => {
   return (
-    <div className="grid">
+    <div className="grid grid-flow-col">
       <div className="col-span-2">
         <h1>{"<Nikhil Rawal/>"}</h1>
       </div>
-      <div className="col-span-1">
+      <div className="flex col-span-1">
         <h1>About</h1>
         <h1>Skills</h1>
         <h1>Portfolio</h1>
         <h1>Contact</h1>
       </div>
-      <div className="col-span-2"></div>
+      <div className="flex col-span-2">
+        <FaSun />
+        <BsFillMoonStarsFill />
+      </div>
     </div>
   );
 };
